fedd13739f6eac5098fd4486ab2eba4b26841da2
a <a>
2026-04-14 10:15:57 +0200

    a

--- sw-register.js
+++ sw-register.js
@@ -1,7 +1,7 @@
-const ROOT = "/";
+const PATHNAME = "/";
 
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register(`${ROOT}sw.js`)
+	navigator.serviceWorker.register(`${PATHNAME}sw.js`)
 		.then(() => console.log("Service Worker registrado"))
 		.catch(err => console.error("Error al registrar SW:", err));
 }
