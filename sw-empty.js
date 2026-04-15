461d82fe30a3e05758c7f3ecbfce985e72ef7fbf
a <a>
2026-04-15 11:26:52 +0200

    a

---
+++ sw-empty.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', () => {
+	// No hace nada
+});
+
+self.addEventListener('activate', () => {
+	// Tampoco hace nada
+});
--- sw-register.js
+++ sw-register.js
@@ -1,7 +1,7 @@
 const PATHNAME = "/";
 
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register(`${PATHNAME}sw.js`)
+	navigator.serviceWorker.register(`${PATHNAME}sw-uninstall.js`)
 		.then(() => console.log("Service Worker registrado"))
 		.catch(err => console.error("Error al registrar SW:", err));
 }
