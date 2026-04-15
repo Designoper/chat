c7be2c43f585083bcd3aca3c6fb3d6f18a44295d
a <a>
2026-04-15 11:40:38 +0200

    a

--- sw-register.js
+++ sw-register.js
@@ -1,7 +1,8 @@
 const PATHNAME = "/";
+const FILENAME = "sw-uninstall.js";
 
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register(`${PATHNAME}sw-uninstall.js`)
+	navigator.serviceWorker.register(`${PATHNAME}${FILENAME}`)
 		.then(() => console.log("Service Worker registrado"))
 		.catch(err => console.error("Error al registrar SW:", err));
 }
