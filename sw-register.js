24630fe57fa976aa8b7445ee41d281acf113bb0a
a <a>
2026-04-24 10:00:10 +0200

    a

--- sw-register.js
+++ sw-register.js
@@ -1,8 +1,10 @@
 const PATHNAME = "/";
 const FILENAME = "sw-empty.js";
 
-if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register(`${PATHNAME}${FILENAME}`)
-		.then(() => console.log("Service Worker registrado"))
-		.catch(err => console.error("Error al registrar SW:", err));
+if (navigator.serviceWorker) {
+	try {
+		await navigator.serviceWorker.register(`${PATHNAME}${FILENAME}`);
+	} catch (err) {
+		console.error("Error al registrar SW:", err);
+	}
 }
