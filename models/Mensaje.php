36e7621c80e9b4f69475be6928313c7179991966
a <a>
2026-06-01 11:20:47 +0200

    a

--- models/Mensaje.php
+++ models/Mensaje.php
@@ -82,6 +82,81 @@ readonly class Mensaje extends MysqliConnect
 			: $this->errors->setValidationError($error_message);
 	}
 
+	// MARK: GET ULTIMO MENSAJE DIRECTO
+
+	private function getUltimoMensajeDirecto(): void
+	{
+		$this->setIdReceptor();
+		$this->checkValidationErrors();
+
+		$id_receptor = $this->id_receptor;
+		$id_usuario = $this->id_emisor;
+
+		$statement =
+			"SELECT
+				nombre_usuario,
+				contenido,
+				DATE_FORMAT(fecha_envio, '%Y-%m-%dT%H:%i:%sZ') AS fecha_envio
+				FROM mensajes
+				LEFT JOIN usuarios ON mensajes.id_emisor = usuarios.id_usuario
+				WHERE
+					(id_emisor = ? AND id_receptor = ?)
+					OR (id_emisor = ? AND id_receptor = ?)
+				AND id_grupo IS NULL
+				ORDER BY fecha_envio DESC
+				LIMIT 1";
+
+		$query = $this->connection->prepare($statement);
+
+		$query->bind_param(
+			"iiii",
+			$id_usuario,
+			$id_receptor,
+			$id_receptor,
+			$id_usuario
+		);
+
+		$query->execute();
+
+		$ultimo_mensaje = $query->get_result()->fetch_assoc();
+
+		$query->close();
+
+		$this->status = 200;
+		$this->message = 'Último mensaje directo obtenido con éxito';
+		$this->content = $ultimo_mensaje;
+		$this->sendResponse();
+	}
+
+	// MARK: GET ULTIMO MENSAJE PUBLICO
+
+	private function getUltimoMensajePublico(): void
+	{
+		$statement =
+			"SELECT contenido,
+				DATE_FORMAT(fecha_envio, '%Y-%m-%dT%H:%i:%sZ') AS fecha_envio,
+				nombre_usuario
+				FROM mensajes
+				LEFT JOIN usuarios ON mensajes.id_emisor = usuarios.id_usuario
+				WHERE id_receptor IS NULL
+				AND id_grupo IS NULL
+				ORDER BY fecha_envio DESC
+				LIMIT 1";
+
+		$query = $this->connection->prepare($statement);
+
+		$query->execute();
+
+		$ultimo_mensaje = $query->get_result()->fetch_assoc();
+
+		$query->close();
+
+		$this->status = 200;
+		$this->message = 'Último mensaje público obtenido con éxito';
+		$this->content = $ultimo_mensaje;
+		$this->sendResponse();
+	}
+
 	// MARK: GET ULTIMO ID
 
 	public function getUltimoIdMensaje(): void
