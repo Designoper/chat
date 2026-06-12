5751c6e27c9a722713d507e1d4b0a7f5255db0fd
a <a>
2026-06-12 10:36:22 +0200

    a

--- assets/js/modules/Grupo.js
+++ assets/js/modules/Grupo.js
@@ -9,78 +9,20 @@ export default class Grupo extends Usuario {
 		super();
 	}
 
-	// async getGruposMiembro() {
-	// 	const response = await this.fetchWithoutForm(this.ENDPOINTS.GET.GRUPOS.MIEMBRO, 'get');
-	// 	return response;
-	// }
-
-	// async printGruposMiembro(grupos) {
-	// 	const content = await this.gruposMiembroTemplate(grupos);
-	// 	return response;
-	// }
-
-	// async getGruposPendiente() {
-	// 	const { json } = await this.fetchWithoutForm(this.ENDPOINTS.GET.GRUPOS.PENDIENTE, 'get');
-	// 	return json;
-	// }
-
-	// async printGruposPendiente() {
-	// 	const response = await this.getGruposPendiente();
-	// 	const content = this.gruposPendienteTemplate(response);
-	// 	this.invitacionesMenu.innerHTML = content;
-	// }
-
 	gruposNoMiembroTemplate(fetchedGrupos) {
 
-		const opciones = fetchedGrupos
+		const noMiembros = fetchedGrupos
 			.map(grupo => `<option translate="no" value="${grupo.id_usuario}">${grupo.nombre_usuario}</option>`)
 			.join('');
 
-		return `<option value="">Añadir a...</option>${opciones}`;
+		const template =
+			`
+			<option value="">Añadir a...</option>
+			${noMiembros}
+			`;
+
+		return template;
 	}
-
-
-	// async gruposMiembroTemplate(fetchedGrupos) {
-
-	// 	const grupos = await Promise.all(
-	// 		fetchedGrupos.map(async grupo => {
-
-	// 			const invitables = await this.fetchWithoutForm(this.ENDPOINTS.GET.GRUPOS.NO_MIEMBRO, 'get',
-	// 				{
-	// 					"id_grupo": grupo.id_grupo
-	// 				}
-	// 			);
-
-	// 			const opciones = invitables.content
-	// 				.map(user => `<option translate="no" value="${user.id_usuario}">${user.nombre_usuario}</option>`)
-	// 				.join('');
-
-	// 			const formInvitar =
-	// 				`<li>
-
-	// 					<a href="./chat.php?id_grupo=${grupo.id_grupo}&nombre_grupo=${grupo.nombre_grupo}">
-	// 						<h2 translate="no">${grupo.nombre_grupo}</h2>
-	// 						${badge}
-	// 						${lastMessage}
-	// 					</a>
-
-	// 					<form method="POST" name="invitar">
-	// 						<input type="hidden" value="${grupo.id_grupo}" name="id_grupo">
-	// 						<select name="id_invitado" id="id_invitado" required>
-	// 							<option value="">Añadir a...</option>
-	// 							${opciones}
-	// 						</select>
-	// 						<button>Invitar</button>
-	// 					</form>
-
-	// 				</li>`;
-
-	// 			return formInvitar;
-	// 		})
-	// 	);
-
-	// 	return grupos.join('');
-	// }
 
 	gruposPendienteTemplate(fetchedGrupos) {
 
