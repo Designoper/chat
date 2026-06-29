ab559d736f8fef39168f8c6e307f03f8f2c6adae
a <a>
2026-06-29 16:59:08 +0200

    a

--- models/Invitacion.php
+++ models/Invitacion.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require_once __DIR__ . '/Grupo.php';
 
-final readonly class Invitacion extends Grupo
+readonly class Invitacion extends Grupo
 {
 	protected int $id_contacto;
 
