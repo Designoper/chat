3f16f39db8492c49cc62dfe2739d0e19e049d04f
a <a>
2026-07-01 18:17:36 +0200

    a

--- database.sql
+++ database.sql
@@ -4,6 +4,8 @@
 
 SET default_storage_engine=InnoDB;
 
+-- MARK: USUARIOS
+
 CREATE TABLE usuarios (
     ulid_usuario CHAR(26) PRIMARY KEY,
     nombre_usuario VARCHAR(20) NOT NULL UNIQUE,
@@ -11,6 +13,8 @@ CREATE TABLE usuarios (
     codigo_contacto CHAR(6) NOT NULL UNIQUE,
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+-- MARK: GRUPOS
 
 CREATE TABLE grupos (
     ulid_grupo CHAR(26) PRIMARY KEY,
@@ -22,6 +26,8 @@ CREATE TABLE grupos (
         REFERENCES usuarios(ulid_usuario)
         ON DELETE SET NULL
 );
+
+-- MARK: INVITACIONES DIRECTAS
 
 CREATE TABLE invitaciones_directas (
     ulid_usuario CHAR(26) NOT NULL,
@@ -38,6 +44,8 @@ CREATE TABLE invitaciones_directas (
         ON DELETE CASCADE
 );
 
+-- MARK: INVITACIONES GRUPALES
+
 CREATE TABLE invitaciones_grupales (
     ulid_usuario CHAR(26) NOT NULL,
     ulid_grupo CHAR(26) NOT NULL,
@@ -53,22 +61,7 @@ CREATE TABLE invitaciones_grupales (
         ON DELETE CASCADE
 );
 
--- CREATE TABLE contactos_directos (
---     ulid_usuario CHAR(26) NOT NULL,
---     ulid_contacto CHAR(26) NOT NULL,
-
---     UNIQUE KEY contacto_directo (ulid_usuario, ulid_contacto),
-
---     UNIQUE (LEAST(ulid_usuario, ulid_contacto), GREATEST(ulid_usuario, ulid_contacto)),
-
---     FOREIGN KEY (ulid_usuario)
---         REFERENCES usuarios(ulid_usuario)
---         ON DELETE CASCADE,
-
---     FOREIGN KEY (ulid_contacto)
---         REFERENCES usuarios(ulid_usuario)
---         ON DELETE CASCADE
--- );
+-- MARK: CONTACTOS DIRECTOS
 
 CREATE TABLE contactos_directos (
     ulid_a CHAR(26) NOT NULL,
@@ -82,6 +75,7 @@ CREATE TABLE contactos_directos (
     FOREIGN KEY (ulid_b) REFERENCES usuarios(ulid_usuario) ON DELETE CASCADE
 );
 
+-- MARK: CONTACTOS GRUPALES
 
 CREATE TABLE contactos_grupales (
     ulid_usuario CHAR(26) NOT NULL,
@@ -97,6 +91,8 @@ CREATE TABLE contactos_grupales (
         REFERENCES grupos(ulid_grupo)
         ON DELETE CASCADE
 );
+
+-- MARK: MENSAJES
 
 CREATE TABLE mensajes (
     ulid_mensaje CHAR(26) PRIMARY KEY,
@@ -120,6 +116,8 @@ CREATE TABLE mensajes (
         ON DELETE SET NULL
 );
 
+-- MARK: ULTIMOS MENSAJES LEIDOS DIRECTOS
+
 CREATE TABLE ultimos_mensajes_leidos_directos (
     ulid_usuario CHAR(26) NOT NULL,
     ulid_contacto CHAR(26) NOT NULL,
@@ -135,6 +133,8 @@ CREATE TABLE ultimos_mensajes_leidos_directos (
         REFERENCES usuarios(ulid_usuario)
         ON DELETE CASCADE
 );
+
+-- MARK: ULTIMOS MENSAJES LEIDOS GRUPALES
 
 CREATE TABLE ultimos_mensajes_leidos_grupales (
     ulid_usuario CHAR(26) NOT NULL,
