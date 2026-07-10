3ca5a0ed6306d7d2648b92ce3d7aa25139566a49
a <a>
2026-07-10 16:44:16 +0200

    a

--- assets/js/modules/Mensaje.js
+++ assets/js/modules/Mensaje.js
@@ -380,9 +380,10 @@ export default class Mensaje extends Contacto {
 				this.dom.form[1].name = 'createMensajeGrupalImagen';
 				this.dom.form[2].name = 'createMensajeGrupalImagen';
 				this.dom.form[3].name = 'createMensajeGrupalAudio';
-				this.dom.form[4].name = 'createMensajeGrupalVideo';
+				this.dom.form[4].name = 'createMensajeGrupalAudio';
 				this.dom.form[5].name = 'createMensajeGrupalVideo';
-				this.dom.form[6].name = 'createMensajeGrupal';
+				this.dom.form[6].name = 'createMensajeGrupalVideo';
+				this.dom.form[7].name = 'createMensajeGrupal';
 			}
 
 			if (this.urlSearchParams.has('ulid_contacto')) {
@@ -391,9 +392,10 @@ export default class Mensaje extends Contacto {
 				this.dom.form[1].name = 'createMensajeDirectoImagen';
 				this.dom.form[2].name = 'createMensajeDirectoImagen';
 				this.dom.form[3].name = 'createMensajeDirectoAudio';
-				this.dom.form[4].name = 'createMensajeDirectoVideo';
+				this.dom.form[4].name = 'createMensajeDirectoAudio';
 				this.dom.form[5].name = 'createMensajeDirectoVideo';
-				this.dom.form[6].name = 'createMensajeDirecto';
+				this.dom.form[6].name = 'createMensajeDirectoVideo';
+				this.dom.form[7].name = 'createMensajeDirecto';
 			}
 		}
 	}
